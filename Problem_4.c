fa123798cceae97f49a18077ac58eff435b13f15
Aniruddha Bagal <bagalaniruddha@gmail.com>
2021-02-17 11:36:48 +0000

    Took input and printed Half Pyramid of n Stars.

--- Problem_4.c
+++ Problem_4.c
@@ -10,6 +10,16 @@ int main()
   	scanf("%d", &n);
 
   	//Write your code here
+
+    for(int i=0; i<n; i++){
+
+      for(int k=0; k<i; k++){
+
+          printf(" * ");
+          
+      }
+      printf("\n");
+    }
   	
 
   	return 0;
